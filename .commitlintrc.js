240a58ef8412b59b6e0eb9d17937b106b1a8d675
Ricardo Pereira <ricardo.mlpp@gmail.com>
2024-03-25 15:50:31 +0000

    chore(config): create commitlint configuration

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+const allowedScopes = ["config", "deps", "repo", "workspace"];
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [2, "never"],
+    "scope-enum": [2, "always", [...allowedScopes].sort()],
+  },
+};
