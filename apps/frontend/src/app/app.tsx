fc2cbf2725f8f8681cac333524ce642df28cc0da
Ricardo Pereira <ricardo.mlpp@gmail.com>
2024-03-25 17:48:34 +0000

    chore(frontend): remove nx welcome component

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,14 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="frontend" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
