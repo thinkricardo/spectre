677664eb09a3912f978de48e03753818e2f1487e
Ricardo Pereira <ricardo.mlpp@gmail.com>
2024-03-25 18:11:30 +0000

    feat(canvas): render multiple shapes

--- apps/frontend/src/app/components/canvas.tsx
+++ apps/frontend/src/app/components/canvas.tsx
@@ -1,9 +1,16 @@
 import styles from './canvas.module.scss';
 
+const shapes = [
+  { x: 50, y: 50 },
+  { x: 250, y: 350 },
+];
+
 export function Canvas() {
   return (
     <svg className={styles.canvas}>
-      <rect x="50" y="50" height="100" width="100" fill="#646cff" />
+      {shapes.map((shape) => (
+        <rect x={shape.x} y={shape.y} height="100" width="100" fill="#646cff" />
+      ))}
     </svg>
   );
 }
