47087c125d2403f40ec3726cd69a4942014b0a2f
Ricardo Pereira <ricardo.mlpp@gmail.com>
2024-03-25 17:54:30 +0000

    feat(frontend): render basic shape

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,7 +2,11 @@
 import styles from './app.module.scss';
 
 export function App() {
-  return <div></div>;
+  return (
+    <svg>
+      <rect x="50" y="50" height="100" width="100" fill="#646cff" />
+    </svg>
+  );
 }
 
 export default App;
