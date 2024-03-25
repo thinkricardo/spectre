345f2790d1273555c7a14c35d636fbc81e3936bd
Ricardo Pereira <ricardo.mlpp@gmail.com>
2024-03-25 17:35:50 +0000

    chore(config): include frontend as allowed scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,9 +1,12 @@
-const allowedScopes = ["config", "deps", "repo", "workspace"];
+const generalScopes = ['config', 'deps', 'repo', 'workspace'];
+const projectScopes = ['frontend'];
+
+const allowedScopes = [...generalScopes, ...projectScopes];
 
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "scope-empty": [2, "never"],
-    "scope-enum": [2, "always", [...allowedScopes].sort()],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', [...allowedScopes].sort()],
   },
 };
