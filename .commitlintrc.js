cd871baae1f315914e115d938c04fac332d4876b
Ricardo Pereira <ricardo.mlpp@gmail.com>
2024-04-05 16:53:00 +0100

    chore(config): include store as allowed scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 const generalScopes = ['config', 'deps', 'repo', 'workspace'];
 
 const projectScopes = ['frontend'];
-const moduleScopes = ['canvas', 'controls', 'shape', 'state'];
+const moduleScopes = ['canvas', 'controls', 'shape', 'state', 'store'];
 
 const allowedScopes = [...generalScopes, ...projectScopes, ...moduleScopes];
 
